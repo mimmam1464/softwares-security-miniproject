143752f15703e4b37927aed9e7147c7bc248da0b
mimmam1464 <m.immam@ufl.edu>
2020-03-06 01:56:13 -0500

    database created and connected

---
+++ CIS4930_Mini_Project_1/CIS4930_Mini_Project_1/Controllers/AccountHController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CIS4930_Mini_Project_1.Controllers
+{
+    public class AccountHController : Controller
+    {
+        // GET: AccountH
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CIS4930_Mini_Project_1/CIS4930_Mini_Project_1/Controllers/AccountRController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CIS4930_Mini_Project_1.Models;
+
+namespace CIS4930_Mini_Project_1.Controllers
+{
+    public class AccountRController : Controller
+    {
+        // GET: AccountR
+        public ActionResult Register(LoginModel model)
+        {
+            //Do not check for any security issues.
+
+            //Do input validation
+            if(model.name == null)
+                ModelState.AddModelError("name","Name cannot be null!");
+            if (model.password == null)
+                ModelState.AddModelError("name", "Password cannot be null!");
+
+            //do null checks before registering in the database
+            return null;
+        }
+    }
+}
