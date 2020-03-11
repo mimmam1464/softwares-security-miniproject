5e2339b9e0195ba5d8fe0459783f05030675057c
mimmam1464 <44204526+mimmam1464@users.noreply.github.com>
2020-03-11 00:19:55 -0400

    synchronizing dashboard

--- CIS4930_Mini_Project_1/CIS4930_Mini_Project_1/Controllers/HomeController.cs
+++ CIS4930_Mini_Project_1/CIS4930_Mini_Project_1/Controllers/HomeController.cs
@@ -31,10 +31,6 @@ namespace CIS4930_Mini_Project_1.Controllers
             return View();
         }
 
-        public ActionResult forgotPass()
-        {
-            return View();
-        }
 
         [HttpGet]
         public ActionResult Dashboard()
